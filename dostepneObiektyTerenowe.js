7dfe398b75cad82c0b1ae6b2bc16bc25f2b73ae8
Jonasz Kozlowski <jonasz.kozlowski@qnt.pl>
2019-01-04 14:00:28 +0100

    comit z della 2

--- dostepneObiektyTerenowe.js
+++ dostepneObiektyTerenowe.js
@@ -1,46 +1,30 @@
 var dostepneObiektyTerenowe = {
 
 
-        obiekty: [
+    obiekty: [
+        {val: 'false', desc: '- wybierz -'},
+        {val: 'droga', desc: 'droga'},
+        {val: 'gmina', desc: 'gmina'},
+        {val: 'granica', desc: 'granica'},
+        {val: 'góra', desc: 'góra'},
+        {val: 'jezioro', desc: 'jezioro'},
+        {val: 'kanał', desc: 'kanał'},
+        {val: 'miasto', desc: 'miasto'},
+        {val: 'pasmo górskie', desc: 'pasmo górskie'},
+        {val: 'pola uprawne', desc: 'pola uprawne'},
+        {val: 'pomnik przyrody', desc: 'pomnik przyrody'},
+        {val: 'rereny leśne', desc: 'tereny leśne'},
+        {val: 'rezerwat przyrody', desc: 'rezerwat przyrody'},
+        {val: 'rzeka', desc: 'rzeka'},
+        {val: 'staw', desc: 'staw'},
+        {val: 'treny rolnicze', desc: 'treny rolnicze'},
+        {val: 'wieś', desc: 'wieś'},
+        {val: 'zabudowania', desc: 'zabudowania'},
+        {val: 'zakład produkcyjny', desc: 'zakład produkcyjny'},
 
 
-
-
-            {val: 'false', desc: '- wybierz -'},
-            {val: 'droga', desc: 'droga'},
-            {val: 'gmina', desc: 'gmina'},
-            {val: 'granica', desc: 'granica'},
-            {val: 'góra', desc: 'góra'},
-            {val: 'jezioro', desc: 'jezioro'},
-            {val: 'kanał', desc: 'kanał'},
-            {val: 'miasto', desc: 'miasto'},
-            {val: 'pasmo górskie', desc: 'pasmo górskie'},
-            {val: 'pola uprawne', desc: 'pola uprawne'},
-            {val: 'pomnik przyrody', desc: 'pomnik przyrody'},
-            {val: 'rereny leśne', desc: 'tereny leśne'},
-            {val: 'rezerwat przyrody', desc: 'rezerwat przyrody'},
-            {val: 'rzeka', desc: 'rzeka'},
-            {val: 'staw', desc: 'staw'},
-            {val: 'treny rolnicze', desc: 'treny rolnicze'},
-            {val: 'wieś', desc: 'wieś'},
-            {val: 'zabudowania', desc: 'zabudowania'},
-            {val: 'zakład produkcyjny', desc: 'zakład produkcyjny'},
-
-
-
-
-
-
-
-
-
-
-
-        ]
-    };
-
-
-
+    ]
+};
 
 
 // pomiary na mapie
